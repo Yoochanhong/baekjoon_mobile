eb1e1eba384b35c70021f2c52beb52957f491cd5
맥북 유찬홍 <chanhongy6@naver.com>
2023-02-09 20:15:00 +0900

    로그인은 다음에 만드는걸로,,

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class LoginPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:baekjoon_mobile/baseurl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:baekjoon_mobile/model/user/user_data.dart';
+import 'package:http/http.dart' as http;
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..loadRequest(Uri.parse('${baseurl}api/v3/account/verify_credentials'));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
